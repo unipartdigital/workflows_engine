77d0a783ef7d1529b4b61b49eb60f7c76c24f5b0
Michele Costa <michele.costa@unipart.io>
2020-09-04 16:39:43 +0100

    Add preliminary jsonschema and test validator
    Signed-off-by: Michele Costa <michele.costa@unipart.io>

--- src/core/tests/__init__.py
+++ src/core/tests/__init__.py
@@ -1,2 +1,4 @@
+from . import validator
 from . import test_flow
 from . import test_workflow
+from . import test_tasks
---
+++ src/core/tests/test_tasks.py
@@ -0,0 +1,20 @@
+from .validator import get_validator_for
+from workflows_engine.core import tasks
+from workflows_engine import validators
+
+
+def test_jsonrpc():
+    task = tasks.JsonRpc(
+        name="task",
+        url="/endpoint/task/url",
+        preconditions=validators.is_equal(value_key="$.value", validator_value="a"),
+        method="GET",
+        payload_paths=[
+            {"key": "$.arg1", "result_key": "$.arg1_result"},
+            {"key": "$.arg2", "result_key": "$.arg2_result"},
+        ],
+        payload={"arg1_result": None, "arg2_result": None},
+        response_path="$.response",
+    )
+    validator = get_validator_for("tasks/jsonrpc")
+    validator.validate(task.as_dict())
