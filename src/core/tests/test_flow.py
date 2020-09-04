9e0b7181d317af41423cc6aa5e3d2fcd56f06db6
Michele Costa <michele.costa@unipart.io>
2020-09-04 16:39:43 +0100

    Add preliminary jsonschema and test validator
    Signed-off-by: Michele Costa <michele.costa@unipart.io>

--- src/core/tests/test_flow.py
+++ src/core/tests/test_flow.py
@@ -1,6 +1,7 @@
 import pytest
 from workflows_engine.core.tasks import Flow
 from workflows_engine import validators
+from .validator import get_validator_for
 
 
 @pytest.fixture
@@ -39,23 +40,5 @@ def test_flow_to_dict(flow):
         payload={"arg1_result": None, "arg2_result": None},
         response_path="$.response",
     )
-
-    assert flow.as_dict() == {
-        "type": "flow",
-        "name": "main_flow",
-        "tasks": [
-            {
-                "type": "jsonrpc",
-                "name": "task",
-                "preconditions": ["equals_value_a"],
-                "url": "/endpoint/task/url",
-                "method": "GET",
-                "payload_paths": [
-                    {"key": "$.arg1", "result_key": "$.arg1_result"},
-                    {"key": "$.arg2", "result_key": "$.arg2_result"},
-                ],
-                "payload": {"arg1_result": None, "arg2_result": None},
-                "response_path": "$.response",
-            }
-        ],
-    }
+    validator = get_validator_for("tasks/flow")
+    validator.validate(flow.as_dict())
--- src/core/tests/test_workflow.py
+++ src/core/tests/test_workflow.py
@@ -1,6 +1,7 @@
 import pytest
 from workflows_engine import Workflow
 from workflows_engine import validators
+from .validator import get_validator_for
 
 
 @pytest.fixture
@@ -25,38 +26,6 @@ def workflow():
 
 
 def test_flow_to_dict(workflow):
-    assert workflow(context={"value": "a"}).as_dict() == {
-        "validators": {
-            "equals_value_a": {
-                "equals_value_a": {
-                    "type": "equals",
-                    "message": {"type": "error", "template": "Error: equals to values"},
-                    "valid_when": True,
-                    "value_key": "$.value",
-                    "validator_value": "a",
-                }
-            }
-        },
-        "components": {},
-        "flow": {
-            "type": "flow",
-            "name": "WorkflowTest",
-            "tasks": [
-                {
-                    "type": "jsonrpc",
-                    "name": "task",
-                    "preconditions": ["equals_value_a"],
-                    "url": "/endpoint/task/url",
-                    "method": "GET",
-                    "payload_paths": [
-                        {"key": "$.arg1", "result_key": "$.arg1_result"},
-                        {"key": "$.arg2", "result_key": "$.arg2_result"},
-                    ],
-                    "payload": {"arg1_result": None, "arg2_result": None},
-                    "response_path": "$.response",
-                }
-            ],
-        },
-        "hash": "8dc828bab468306772d69c761b5395768903add89133d783a25265c555e2916230c0efddafb48e19c2509b049c07b586d929fa624c8bcbb0319ee2abb1762dfa",
-        "context": {"value": "a"},
-    }
+    workflow_dict = workflow(context={"value": "a"}).as_dict()
+    validator = get_validator_for("workflow")
+    validator.validate(workflow_dict)
