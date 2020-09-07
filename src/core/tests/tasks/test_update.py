78d36254eab891d95cb9d3213cc44d5d94517037
Michele Costa <michele.costa@unipart.io>
2020-09-07 22:16:27 +0100

    Fix schema for update task
    Signed-off-by: Michele Costa <michele.costa@unipart.io>

---
+++ src/core/tests/tasks/test_update.py
@@ -0,0 +1,17 @@
+from ..validator import get_validator_for
+from workflows_engine.core import tasks, components
+from workflows_engine import validators
+
+
+def test_update():
+    task = tasks.Update(
+        name="update",
+        preconditions=validators.is_equal(value_key="$.value", validator_value="a"),
+        tasks=[
+            {"key": "$.src", "result_key": "$.dest"},
+            {"result": True, "result_key": "$.dest"},
+            {"template": "This is the source: {$.src}", "result_key": "$.dest"},
+        ],
+    )
+    validator = get_validator_for("tasks/update")
+    validator.validate(task.as_dict())
