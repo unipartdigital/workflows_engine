337ee9727f48790c6c9e43f68a932d93dc0d545b
Michele Costa <michele.costa@unipart.io>
2020-09-07 21:12:52 +0100

    Add more tests
    Signed-off-by: Michele Costa <michele.costa@unipart.io>

---
+++ src/core/tests/tasks/test_screen.py
@@ -0,0 +1,17 @@
+from ..validator import get_validator_for
+from workflows_engine.core import tasks, components
+from workflows_engine import validators
+
+
+def test_screen():
+    task = tasks.Screen(
+        name="condition",
+        components=[
+            components.Button(text="Submit", action="submit", style="primary"),
+            components.Button(text="Next", action="next", style="secondary"),
+        ],
+        status_msg_template="The screen completed",
+        show_status_msg=True,
+    )
+    validator = get_validator_for("tasks/screen")
+    validator.validate(task.as_dict())
