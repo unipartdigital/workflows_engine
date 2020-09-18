ae6ca669a7f96f3ac9955c8661e61ea22545c04a
Michele Costa <michele.costa@unipart.io>
2020-09-18 09:26:31 +0100

    Add translate to __init__
    Signed-off-by: Michele Costa <michele.costa@unipart.io>

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,9 +1,11 @@
+from . import translate
 from . import validators
 from . import components
 from . import tasks
 from . import workflows
 
 __all__ = (
+    "translate",
     "validators",
     "components",
     "tasks",
