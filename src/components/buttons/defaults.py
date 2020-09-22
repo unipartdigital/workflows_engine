58f74489994783d00dbe549ef116f3d2ae2fd1e5
Michele Costa <michele.costa@unipart.io>
2020-09-22 13:26:51 +0100

    Set default style for next button to primary
    Signed-off-by: Michele Costa <michele.costa@unipart.io>

--- src/components/buttons/defaults.py
+++ src/components/buttons/defaults.py
@@ -6,6 +6,6 @@ __all__ = (
 )
 
 submit = dict(identifier="submit_button", action="submit", style="primary", text="Submit",)
-next = dict(identifier="next_button", action="next", style="secondary", text="Next",)
+next = dict(identifier="next_button", action="next", style="primary", text="Next",)
 back = dict(identifier="back_button", action="back", style="default", text="Back",)
 reset = dict(identifier="restart_button", action="reset", style="primary", text="Continue",)
