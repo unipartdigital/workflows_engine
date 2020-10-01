bd0f5cf080370a3d78058fc18c9a84d80c90a163
kenvac <kin.vachhani@gmail.com>
2020-10-01 10:01:37 +0100

    [IMP] workflows_engine: remove unnecessary code from class InvalidArguments

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,6 +1,3 @@
 class InvalidArguments(Exception):
     """ Unwanted arguments.
     Example: When you try to use 'path' and 'value' on input field where only one is allowed."""
-
-    def __init__(self, msg):
-        pass
