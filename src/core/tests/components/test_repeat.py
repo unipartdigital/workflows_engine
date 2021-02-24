99df6ee2cff7d327f5ccc9b563bbd11548e11267
Peter Alabaster <peter.alabaster@unipart.io>
2021-02-24 10:51:15 +0000

    [REFACTOR] Tweak Repeat component attributes
    Rename quantity & quantity_path to times_to_repeat, times_to_repeat_path
      from Repeat component
    Remove validators from Repeat component
    Add destination_path to Repeat component
    Handle component rows in Repeat component
    
    Signed-off-by: Peter Alabaster <peter.alabaster@unipart.io>

--- src/core/tests/components/test_repeat.py
+++ src/core/tests/components/test_repeat.py
@@ -11,19 +11,11 @@ def field():
     return Input(label="Input")
 
 
-@pytest.fixture
-def component_validator():
-    return validators.is_equal(
-        validator_value=["ExpectedValue1", "ExpectedValue2", "ExpectedValue3"]
-    )
-
-
-def test_repeat(field, component_validator):
+def test_repeat(field):
     """Test repeat component creates json matching json schema"""
     repeat = Repeat(
-        components=field,
-        validators=component_validator,
-        quantity=3,
+        components=[field],
+        times_to_repeat=3,
         destination_path="$.result",
     )
     validator = get_validator_for("components/repeat")
@@ -35,7 +27,6 @@ def test_repeat_missing_required_arg(field):
     with pytest.raises(InvalidArguments):
         Repeat(
             components=field,
-            validators=component_validator,
             destination_path="$.result",
         )
 
@@ -45,8 +36,7 @@ def test_repeat_no_missing_mutually_exclusive_args(field):
     with pytest.raises(InvalidArguments):
         Repeat(
             components=field,
-            validators=component_validator,
-            quantity=3,
-            quantity_path="$.quantity",
+            times_to_repeat=3,
+            times_to_repeat_path="$.quantity",
             destination_path="$.result",
         )
