ac6815cda76e6418c76ec75276cb3217001a9f4d
Armand Cela <armand.cela@unipart.io>
2021-03-15 13:06:35 +0000

    [ADD] workflows_engine: Add default in displays type
    Story: 12655
    Signed-off-by: Armand Cela <armand.cela@unipart.io>

--- src/components/displays/defaults.py
+++ src/components/displays/defaults.py
@@ -1,10 +1,12 @@
 __all__ = (
+    "default",
     "info",
     "error",
     "warning",
     "success",
 )
 
+default = dict(identifier="default_message", message_type="default", template="This is question mark")
 info = dict(identifier="info_message", message_type="info", template="This is infomation")
 warning = dict(identifier="info_message", message_type="warning", template="This is a warning")
 error = dict(identifier="info_message", message_type="error", template="Something went wrong")
--- src/components/displays/displays.py
+++ src/components/displays/displays.py
@@ -4,12 +4,14 @@ from . import defaults
 
 
 __all__ = (
+    "default",
     "info",
     "warning",
     "error",
     "success",
 )
 
+default = func_factory(defaults.default, components.MessageBox, make_identifier)
 info = func_factory(defaults.info, components.MessageBox, make_identifier)
 warning = func_factory(defaults.warning, components.MessageBox, make_identifier)
 error = func_factory(defaults.error, components.MessageBox, make_identifier)
