0f447d392795eb39f4b920bc7f4670d77e328f20
Blommaerts Edwin <edwin.blommaerts@gmail.com>
2021-05-10 12:07:45 +0100

    [FIX][SQUASH] workflows_engine Fix typo and remove unneeded tests.

--- src/core/tests/components/test_option_list.py
+++ src/core/tests/components/test_option_list.py
@@ -16,32 +16,3 @@ def test_optionlist_data_path():
     validator = get_validator_for("components/option_list")
     validator.validate(optionlist.get_base_component_dict())
 
-
-# def test_optionlist_missing_required_arg():
-#     """Test missing required argument correctly raises issue"""
-#     with pytest.raises(InvalidArguments):
-#         OptionList(
-#             title="OptionList Title",
-#             destination_path="$.result",
-#             display_type="details",
-#         )
-
-
-# def test_checkboxlist_mutually_exclusive_args():
-#     """Test including mutually exclusive args correctly raises issue"""
-#     with pytest.raises(InvalidArguments):
-#         CheckboxList(
-#             title="CheckboxList Title",
-#             destination_path="$.result",
-#             data_path="$.checkboxlist_data_location",
-#             data=[
-#                 {
-#                     "label": "Label of checkbox 1",
-#                     "value": "Value 1",
-#                 },
-#                 {
-#                     "label": "Label of checkbox 2",
-#                     "value": "Value 2",
-#                 },
-#             ],
-#         )
