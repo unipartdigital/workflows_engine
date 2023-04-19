c26e7d6aa955af776435a87a7d68cd68ccf03f75
Armand Cela <armand.cela@unipart.io>
2023-04-19 11:53:00 +0100

    [ADD] workflows_engine: Adding close action to buttons in workflows engine
    Story: 3612
    Signed-off-by: Armand Cela <armand.cela@unipart.io>

--- src/components/buttons/defaults.py
+++ src/components/buttons/defaults.py
@@ -3,9 +3,11 @@ __all__ = (
     "next",
     "back",
     "reset",
+    "close",
 )
 
 submit = dict(identifier="submit_button", action="submit", style="primary", text="Submit",)
 next = dict(identifier="next_button", action="next", style="primary", text="Next",)
 back = dict(identifier="back_button", action="back", style="default", text="Back",)
 reset = dict(identifier="restart_button", action="reset", style="primary", text="Continue",)
+close = dict(identifier="close_button", action="close", style="primary", text="Close",)
