79f67771873767b1838cbff86b3d795ccf6a1724
Armand Cela <armand.cela@unipart.io>
2024-05-16 13:38:54 +0100

    [ADD] workflows_engine: Changes on engine that are passed to front end related with styling
    Container Component
    Image Component
    MessageBox Component
    Modal Component
    
    SE-1095
    Signed-off-by: Armand Cela <armand.cela@unipart.io>

--- src/components/displays/defaults.py
+++ src/components/displays/defaults.py
@@ -6,8 +6,33 @@ __all__ = (
     "success",
 )
 
-default = dict(identifier="default_message", message_type="default", template="This is question mark")
-info = dict(identifier="info_message", message_type="info", template="This is infomation")
-warning = dict(identifier="info_message", message_type="warning", template="This is a warning")
-error = dict(identifier="info_message", message_type="error", template="Something went wrong")
-success = dict(identifier="success_screen", message_type="success", template="Success")
+default = dict(
+    identifier="default_message",
+    message_type="default",
+    template="This is question mark",
+    background_color="unset",
+)
+info = dict(
+    identifier="info_message",
+    message_type="info",
+    template="This is infomation",
+    background_color="#d9d9d9",
+)
+warning = dict(
+    identifier="info_message",
+    message_type="warning",
+    template="This is a warning",
+    background_color="#ffba19",
+)
+error = dict(
+    identifier="info_message",
+    message_type="error",
+    template="Something went wrong",
+    background_color="#ff1314",
+)
+success = dict(
+    identifier="success_screen",
+    message_type="success",
+    template="Success",
+    background_color="#66fd7f",
+)
